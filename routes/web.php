cabab1be5d0f75c058919f696f4abb206e5d31b7
Utilisateur <micktc366@gmail.com>
2022-11-25 10:07:38 +0100

    remove vite

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,30 @@ use App\Http\Controllers\TicketController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware('auth')->group(function () {
+
    Route::get('/tickets', [TicketController::class, 'index'])->name('ticket.index');
    Route::get('/tickets/create', [TicketController::class, 'create'])->name('ticket.create');
    Route::get('/tickets/edit/{id}', [TicketController::class, 'edit'])->name('ticket.edit');
    Route::post('/tickets/store', [TicketController::class, 'store'])->name('ticket.store');
-});
 
 
- Route::get('/', function () {
-     return Inertia::render('Welcome', [
-         'canLogin' => Route::has('login'),
-         'canRegister' => Route::has('register'),
-         'laravelVersion' => Application::VERSION,
-         'phpVersion' => PHP_VERSION,
-     ]);
- });
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+ //        'phpVersion' => PHP_VERSION,
+ //    ]);
+ //});
 
- Route::get('/dashboard', function () {
-     return Inertia::render('Dashboard');
- })->middleware(['auth', 'verified'])->name('dashboard');
+ //Route::get('/dashboard', function () {
+ //    return Inertia::render('Dashboard');
+ //})->middleware(['auth', 'verified'])->name('dashboard');
 
- Route::middleware('auth')->group(function () {
-     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
- });
+ //Route::middleware('auth')->group(function () {
+ //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+ //    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+ //    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+ //});
 
- require __DIR__.'/auth.php';
+ //require __DIR__.'/auth.php';
